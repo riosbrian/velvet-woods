039779e56e77653be8fd2d8abf9c35951833aa92
brian rios <riosbrian.ar@gmail.com>
2026-02-25 12:38:08 +0100

    create button and spinner component & add lucide-icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ export default function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <Button onClick={() => console.log('Clicked!')}>Click Me!</Button>
+      <div className="flex items-center gap-4 p-8">
+        <Button onClick={() => console.log('Clicked!')}>Primary</Button>
+        <Button variant={'secondary'} onClick={() => console.log('Clicked!')}>
+          Secondary
+        </Button>
+        <Button
+          isLoading={true}
+          variant={'ghost'}
+          onClick={() => console.log('Clicked!')}
+        >
+          Ghost
+        </Button>
+      </div>
     </>
   );
 }
--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,30 +1,43 @@
 import { cn } from '@utils/cn';
 import { cva, type VariantProps } from 'class-variance-authority';
+import Spinner from '@ui/components/Spinner/Spinner';
 
-const variants = cva('cursor-pointer py-2 px-4 rounded-sm font-semibold', {
-  variants: {
-    variant: {
-      primary: '',
-      secondary: '',
+const variants = cva(
+  'cursor-pointer active:scale-95 border py-2 px-4 rounded-sm font-medium transition-all duration-300 ease-in-out',
+  {
+    variants: {
+      variant: {
+        primary:
+          'bg-green-500 border-green-500 text-white hover:bg-green-600 hover:border-green-600 hover:shadow-lg hover:shadow-green-500/30',
+        secondary:
+          'bg-transparent border-green-500 text-green-600 hover:bg-green-50/50',
+        ghost:
+          'border-transparent bg-transparent text-green-600 hover:border-green-500 hover:bg-green-50/30',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
     },
   },
-  defaultVariants: {
-    variant: 'primary',
-  },
-});
+);
 
-type ButtonProps = React.HTMLAttributes<HTMLButtonElement> &
-  VariantProps<typeof variants>;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof variants> & { isLoading?: boolean };
 
 export default function Button({
   children,
   className,
+  isLoading,
   variant,
   ...props
 }: ButtonProps) {
   return (
-    <button {...props} className={cn(variants({ variant }), className)}>
-      {children}
+    <button
+      disabled={isLoading}
+      {...props}
+      className={cn(variants({ variant }), className)}
+    >
+      {!isLoading ? children : <Spinner />}
     </button>
   );
 }
